bc4530bf5821484129412ec8bd1be1360f75a87b
sahusa <sahusa.vinicius@gmail.com>
2019-05-15 18:13:31 -0300

    Versão 1.0.1

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/gerador', (req, res) => {
+    const { bairro, equipe, modelo, placa } = req.query
+    res.render('gerador', {
+        bairro,
+        equipe,
+        modelo,
+        placa
+    })
+})
+
+app.listen(3000, err => {
+    if(err){
+        console.log('Erro no servidor')
+    }else{
+        console.log('Tudo certo na ICM')
+    }
+})
