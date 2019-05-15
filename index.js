6a2ea23e581323d69ab6fe32ebd84bfad81393f0
sahusa <sahusa.vinicius@gmail.com>
2019-05-15 18:22:56 -0300

    troquei a iamgem de fundo e upei o now server no zeit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+const port = process.env.PORT || 3000
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
@@ -20,7 +22,7 @@ app.get('/gerador', (req, res) => {
     })
 })
 
-app.listen(3000, err => {
+app.listen(port, err => {
     if(err){
         console.log('Erro no servidor')
     }else{
